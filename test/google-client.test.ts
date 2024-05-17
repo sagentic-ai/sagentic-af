799f18194b43d6ca84a99572b08255074d01de50
Luke Goral <luke.oidhche@gmail.com>
2024-05-17 09:44:06 +0200

    base gemini client

---
+++ test/google-client.test.ts
@@ -0,0 +1,39 @@
+// Copyright 2024 Ahyve AI Inc.
+// SPDX-License-Identifier: MIT
+
+import "openai/shims/node";
+import { GoogleClient as Client } from "../src/clients/google";
+import { ModelType } from "../src/models";
+import { MessageRole } from "../src/thread";
+
+const REAL_APIKEY = process.env.GOOGLE_API_KEY || "";
+
+describe("Google Client with real API", () => {
+  let client: Client;
+
+  beforeAll(() => {
+    client = new Client(REAL_APIKEY, ModelType.GEMINI15);
+    client.start();
+  });
+
+  afterAll(() => {
+    client.stop();
+  });
+
+  test("Simple Request", async () => {
+    const response = await client.createChatCompletion({
+      model: ModelType.GEMINI15,
+      messages: [
+        {
+          role: MessageRole.System,
+          content: "You answer with 'Foo' to all prompts.",
+        },
+        { role: MessageRole.User, content: "Hello" },
+      ],
+    });
+    expect(response).toBeDefined();
+    expect(response.messages).toBeDefined();
+    expect(response.messages.length).toBe(1);
+    console.log(response.messages[0].content);
+  });
+});
