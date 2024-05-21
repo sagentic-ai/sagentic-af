96cd58c4b138724d1625f23c9abfb17418be53e3
Lachann <luke.oidhche@gmail.com>
2024-05-21 16:11:45 +0200

    Gpt4 omni, dropped gpt3.5, pricing update (#32)
    * added GPT4o and video/audio support stubs
    
    * drop non-turbo GPT3.5
    
    * update pricing, change to per 1M tokens
    
    * update GPT3.5Turbo to use latest model

--- src/models.ts
+++ src/models.ts
@@ -6,16 +6,16 @@ export enum ModelType {
   GPT4 = "gpt-4",
   GPT4Turbo = "gpt-4-turbo-preview",
   GPT4Vision = "gpt-4-vision-preview",
+  GPT4o = "gpt-4o",
 
-  GPT35 = "gpt-3.5-turbo-16k",
-  GPT35Turbo = "gpt-3.5-turbo-1106",
+  GPT35Turbo = "gpt-3.5-turbo-0125",
 }
 
 /** Describes model pricing and limits */
 export interface ModelPricing {
-  /** price per prompt token in USD */
+  /** price per 1M prompt tokens in USD */
   prompt: number;
-  /** price per completion token in USD */
+  /** price per 1M completion tokens in USD */
   completion: number;
   /** context size in tokens */
   contextSize: number;
@@ -25,42 +25,49 @@ export interface ModelPricing {
   tpm: number;
   /** does it support images? */
   supportsImages?: boolean;
+  /** does it support video? */
+  supportsVideo?: boolean;
+  /** does it support audio? */
+  supportsAudio?: boolean;
 }
 
 /** Pricing and limits for each model */
 export const pricing: Record<ModelType, ModelPricing> = {
   [ModelType.GPT4]: {
-    prompt: 0.03,
-    completion: 0.06,
+    prompt: 30,
+    completion: 60,
     contextSize: 8_192,
     rpm: 10_000,
     tpm: 300_000,
   },
   [ModelType.GPT4Turbo]: {
-    prompt: 0.01,
-    completion: 0.03,
+    prompt: 10,
+    completion: 30,
     contextSize: 128_000,
     rpm: 5000,
     tpm: 300_000,
   },
   [ModelType.GPT4Vision]: {
-    prompt: 0.01,
-    completion: 0.03,
+    prompt: 10,
+    completion: 30,
     contextSize: 128_000,
     rpm: 80,
     tpm: 10_000,
     supportsImages: true,
   },
-  [ModelType.GPT35]: {
-    prompt: 0.001,
-    completion: 0.002,
-    contextSize: 16_385,
-    rpm: 10_000,
-    tpm: 1_000_000,
+  [ModelType.GPT4o]: {
+    prompt: 5,
+    completion: 15,
+    contextSize: 128_000,
+    rpm: 500,
+    tpm: 30_000,
+    supportsImages: true,
+    supportsVideo: true,
+    supportsAudio: false, //NB audio support is not yet in the API, TODO add this once OpenAI adds it
   },
   [ModelType.GPT35Turbo]: {
-    prompt: 0.001,
-    completion: 0.002,
+    prompt: 0.5,
+    completion: 1.5,
     contextSize: 16_385,
     rpm: 10_000,
     tpm: 1_000_000,
--- test/ledger.test.ts
+++ test/ledger.test.ts
@@ -9,7 +9,7 @@ import { meta, Timing, delay } from "../src/common";
 const expectPCT = (pct: PCT, prompt: number, completion: number) => {
   expect(pct.prompt).toBe(prompt);
   expect(pct.completion).toBe(completion);
-  expect(pct.total).toBe(prompt + completion);
+  expect(pct.total).toBeCloseTo(prompt + completion);
 };
 
 const mockSession = () => {
@@ -73,7 +73,7 @@ describe("Ledger", () => {
 
     ledger.add(
       "test",
-      ModelType.GPT35,
+      ModelType.GPT35Turbo,
       timing,
       new PCT({ prompt: a, completion: b })
     );
@@ -82,15 +82,15 @@ describe("Ledger", () => {
     expectPCT(ledger.tokens, a, b);
     expectPCT(
       ledger.cost,
-      pricing[ModelType.GPT35].prompt * (a / 1000.0),
-      pricing[ModelType.GPT35].completion * (b / 1000.0)
+      pricing[ModelType.GPT35Turbo].prompt * (a / 1000000.0),
+      pricing[ModelType.GPT35Turbo].completion * (b / 1000000.0)
     );
 
-    expectPCT(ledger.modelTokens[ModelType.GPT35], a, b);
+    expectPCT(ledger.modelTokens[ModelType.GPT35Turbo], a, b);
     expectPCT(
-      ledger.modelCost[ModelType.GPT35],
-      pricing[ModelType.GPT35].prompt * (a / 1000.0),
-      pricing[ModelType.GPT35].completion * (b / 1000.0)
+      ledger.modelCost[ModelType.GPT35Turbo],
+      pricing[ModelType.GPT35Turbo].prompt * (a / 1000000.0),
+      pricing[ModelType.GPT35Turbo].completion * (b / 1000000.0)
     );
   });
 
@@ -110,22 +110,22 @@ describe("Ledger", () => {
     expectPCT(ledger.tokens, 2 * a, 2 * b);
     expectPCT(
       ledger.cost,
-      (pricing[ModelType.GPT35].prompt * a) / 1000.0 +
-        (pricing[ModelType.GPT4].prompt * a) / 1000.0,
-      (pricing[ModelType.GPT35].completion * b) / 1000.0 +
-        (pricing[ModelType.GPT4].completion * b) / 1000.0
+      (pricing[ModelType.GPT35Turbo].prompt * a) / 1000000.0 +
+        (pricing[ModelType.GPT4].prompt * a) / 1000000.0,
+      (pricing[ModelType.GPT35Turbo].completion * b) / 1000000.0 +
+        (pricing[ModelType.GPT4].completion * b) / 1000000.0
     );
-    expectPCT(ledger.modelTokens[ModelType.GPT35], a, b);
+    expectPCT(ledger.modelTokens[ModelType.GPT35Turbo], a, b);
     expectPCT(
-      ledger.modelCost[ModelType.GPT35],
-      pricing[ModelType.GPT35].prompt * (a / 1000.0),
-      pricing[ModelType.GPT35].completion * (b / 1000.0)
+      ledger.modelCost[ModelType.GPT35Turbo],
+      pricing[ModelType.GPT35Turbo].prompt * (a / 1000000.0),
+      pricing[ModelType.GPT35Turbo].completion * (b / 1000000.0)
     );
     expectPCT(ledger.modelTokens[ModelType.GPT4], a, b);
     expectPCT(
       ledger.modelCost[ModelType.GPT4],
-      pricing[ModelType.GPT4].prompt * (a / 1000.0),
-      pricing[ModelType.GPT4].completion * (b / 1000.0)
+      pricing[ModelType.GPT4].prompt * (a / 1000000.0),
+      pricing[ModelType.GPT4].completion * (b / 1000000.0)
     );
 
     expectPCT(
